692855585c5789ce521df9c4392274dadbcd1735
taktojakuba <112001303+taktojakuba@users.noreply.github.com>
2026-09-20 12:04:11 +0200

    fix bash path

--- modules/base.nix
+++ modules/base.nix
@@ -7,6 +7,11 @@
 	networking.networkmanager.enable = true;
 	time.timeZone = "Europe/Warsaw";
 	services.openssh.enable = true;
+	systemd.tmpfiles.rules = [
+		"d /bin 0755 root root -"
+		"L+ /bin/bash - - - - ${pkgs.bash}/bin/bash"
+	];
+	environment.binsh = "/run/current-system/sw/bin/bash";
 	nixpkgs.config.allowUnfree = true;
 	system.stateVersion = "26.05";
 }
