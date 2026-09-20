a0111e1586105366fab6da5866f277be94e90c34
taktojakuba <112001303+taktojakuba@users.noreply.github.com>
2026-09-20 17:20:26 +0200

    add dwl overide

--- modules/display.nix
+++ modules/display.nix
@@ -1,5 +1,15 @@
 { config, lib, pkgs, ... }:
 
+let
+	dwlLaunch = pkgs.writeShellScriptBin "dwl-launch" ''
+		export XCURSOR_THEME=Future-dark-cursors
+		export XCURSOR_SIZE=24
+		export XDG_CURRENT_DESKTOP=dwl:wlroots
+		export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus"
+		export WLR_RENDERER=gles2
+		exec ${config.programs.dwl.package}/bin/dwl -s /home/kuba/dwl/scripts/autostart.sh
+	'';
+in
 {
 	services.xserver = {
 		enable = false;
@@ -18,7 +28,7 @@
 		enable = true;
 		settings = {
 			default_session = {
-				command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd /home/kuba/dwl/scripts/launch.sh";
+				command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd ${dwlLaunch}/bin/dwl-launch";
 				user = "greeter";
 			};
 		};
---
+++ modules/dwl.nix
@@ -0,0 +1,17 @@
+{ config, lib, pkgs, ... }:
+
+{
+	programs.dwl = {
+		enable = true;
+		package = (pkgs.dwl.override {
+			wlroots_0_19 = pkgs.wlroots_0_20;
+		}).overrideAttrs {
+			src = pkgs.fetchFromGitHub {
+				owner = "taktojakuba";
+				repo = "dwl";
+				rev = "4e54d2cc3e421fb4f294b7586546a4f6c32e6630";
+				hash = "sha256-Mna51r8OsCwoliG5a8laLFTgV8x+XmEuMWcBfodSwQs=";
+			};
+		};
+	};
+}
