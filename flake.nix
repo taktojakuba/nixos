542b6c707608bb526eb68e5c140ccef91a3c7479
taktojakuba <112001303+taktojakuba@users.noreply.github.com>
2026-09-20 19:21:31 +0200

    add spotifast as flake

--- flake.nix
+++ flake.nix
@@ -2,10 +2,12 @@
   description = "nixasus configuration";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    spotifast.url = "github:crmne/spotifast";
   };
-  outputs = { self, nixpkgs }: {
-      nixosConfigurations.nixasus = nixpkgs.lib.nixosSystem {
-          modules = [ ./configuration.nix ];
+  outputs = { self, nixpkgs, spotifast }: {
+    nixosConfigurations.nixasus = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit spotifast; };
+      modules = [ ./configuration.nix ];
     };
   };
 }
