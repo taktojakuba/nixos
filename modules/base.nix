3874d380542ce9576b971f5bf23d41609f67e3f3
taktojakuba <112001303+taktojakuba@users.noreply.github.com>
2026-09-20 18:29:02 +0200

    add flakes support

--- modules/base.nix
+++ modules/base.nix
@@ -13,5 +13,6 @@
 	];
 	environment.binsh = "/run/current-system/sw/bin/bash";
 	nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 	system.stateVersion = "26.05";
 }
