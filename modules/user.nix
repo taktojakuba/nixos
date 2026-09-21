6f541ec8b40127b7c33111f07c1f824e3c84f5f2
taktojakuba <112001303+taktojakuba@users.noreply.github.com>
2026-09-21 21:03:27 +0200

    make poweroff reboot and suspend not require sudo

--- modules/user.nix
+++ modules/user.nix
@@ -10,6 +10,25 @@
 		];
 	};
 
+	security.polkit.extraConfig = ''
+		polkit.addRule(function(action, subject) {
+			if (
+				subject.user == "kuba"
+				&& (
+					action.id == "org.freedesktop.login1.power-off"
+					|| action.id == "org.freedesktop.login1.power-off-multiple-sessions"
+					|| action.id == "org.freedesktop.login1.reboot"
+					|| action.id == "org.freedesktop.login1.reboot-multiple-sessions"
+					|| action.id == "org.freedesktop.login1.suspend"
+					|| action.id == "org.freedesktop.login1.suspend-multiple-sessions"
+					|| action.id == "org.freedesktop.login1.halt"
+				)
+			) {
+				return polkit.Result.YES;
+			}
+		});
+	'';
+
 	programs.zsh = {
 		enable = true;
 		enableCompletion = true;
