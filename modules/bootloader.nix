fea86c97c56df1b1d926cb0ebdbf752a2e3b55f6
taktojakuba <112001303+taktojakuba@users.noreply.github.com>
2026-09-23 18:34:52 +0200

    configure bootloader.nix

--- modules/bootloader.nix
+++ modules/bootloader.nix
@@ -2,6 +2,17 @@
 
 {
   boot.loader.limine.enable = true;
-  boot.loader.limine.efiSupport = true;
+  boot.loader.limine.resolution = "1920x1080x32";
+  boot.loader.limine.enableEditor = false;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.loader.limine.style = {
+    wallpapers = [ ];
+    backdrop = "000000";
+    interface.helpHidden = true;
+    graphicalTerminal = {
+      margin = 0;
+      marginGradient = 0;
+    };
+  };
 }
