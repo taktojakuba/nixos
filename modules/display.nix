4578d5dac386427445799390a990dcfd61c03d60
taktojakuba <112001303+taktojakuba@users.noreply.github.com>
2026-09-24 19:53:34 +0200

    add dwl launch to displa.nix

--- modules/display.nix
+++ modules/display.nix
@@ -28,7 +28,7 @@ in
 		enable = true;
 		settings = {
 			default_session = {
-				command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd ${dwlLaunch}/bin/dwl-launch";
+				command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd /home/kuba/dwl/scripts/launch.sh";
 				user = "kuba";
 			};
 		};
