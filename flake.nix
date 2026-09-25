1e811a0484b499c7e0853c1785aa0c135d059e53
taktojakuba <112001303+taktojakuba@users.noreply.github.com>
2026-09-25 15:39:47 +0200

    update

--- flake.nix
+++ flake.nix
@@ -3,27 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-
-  outputs =
-    inputs@{ nixpkgs, home-manager, ... }:
-    {
-      nixosConfigurations = {
-        nixasus = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./configuration.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = { inherit inputs; };
-              home-manager.users.kuba = ./modules/home.nix;
-            }
-          ];
-        };
-      };
+  outputs = { nixpkgs, ...}: {
+    nixossConfiguration.nixasus = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
     };
+  };
 }
