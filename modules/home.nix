8142e01115140f11bb510448d35cdc9600d21579
taktojakuba <112001303+taktojakuba@users.noreply.github.com>
2026-09-25 18:26:49 +0200

    modularize home-manager

--- modules/home.nix
+++ modules/home.nix
@@ -1,37 +1,9 @@
 { config, pkgs, ...}: {
+  imports = [ ../apps/zsh.nix ];
+
   home = {
     username = "kuba";
     homeDirectory = "/home/kuba";
     stateVersion = "26.05";
-  };
-  programs.zsh = {
-    enable = true;
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "direnv", "docker", "foot" ];
-      theme = "avit";
-    };
-    initContent = "
-      cs() { cd '$@' && ls; }
-      memof() {
-      pid=$(pidof $1)
-      grep '^Pss:' /proc/$pid/smaps_rollup | awk '{printf '%.2f MiB\n', $2/1024}'
-      }
-    ";
-    shellAliases = {
-      rebuild-home = "nh home switch";
-      rebuild = "nh os switch";
-      vim = "nvim";
-      ga="git add .";
-      gi="git init";
-      gc="git commit -m";
-      gp="git push";
-      gs="git status";
-      gu="git pull";
-      mkdir="mkdir -p";
-      gpp="g++";
-      ls="ls -ah --color=always --group-directories-first";
-      opencode="nix run nixpkgs#opencode --extra-experimental-features nix-command --extra-experimental-features flakes";
-    };
   };
 }
